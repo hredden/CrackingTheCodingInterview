6c6f35d47056177c3b0d80660d8dde975b4afc8c
hredden <max.redden@gmail.com>
2014-12-31 17:44:52 +0100

    "Initial Commit, unique string problem"

---
+++ src/ctci/CTCI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ctci;
+
+/**
+ *
+ * @author Max
+ * Used for testing practice questions
+ */
+public class CTCI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        /*test unique chars in string*/
+        UniqueStringChars.isUnique("A Dogg went");
+        
+    }
+    
+}
